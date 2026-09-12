b3fcec8ebe0fb36e7062108e0e664b7c73da877d
tushar9090s-netizen <tushar9090s@gmail.com>
2026-09-12 17:36:11 +0530

    set up firebase auth

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import '../backend/src/config/firebase.js';
 import SystemWindow from './SystemWindow.jsx';
 
 createRoot(document.getElementById('root')).render(
